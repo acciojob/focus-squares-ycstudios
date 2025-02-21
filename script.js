21203ebc697e01cd7838283bca9d91540f84775e
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-21 21:57:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-//your JS code here. If required.
 document.querySelectorAll('.square').forEach(square => {
     square.addEventListener('mouseover', (e) => {
         document.querySelectorAll('.square').forEach(sq => {
             if (sq !== e.target) {
-                sq.style.background = '#6F4E37'; // Coffee color
+                sq.style.backgroundColor = 'rgb(111, 78, 55)'; // Coffee color
             }
         });
     });
 
     square.addEventListener('mouseleave', () => {
         document.querySelectorAll('.square').forEach(sq => {
-            sq.style.background = 'rgb(230, 230, 250)'; // Reset to Lavender
+            sq.style.backgroundColor = 'rgb(230, 230, 250)'; // Reset to Lavender
         });
     });
 });
